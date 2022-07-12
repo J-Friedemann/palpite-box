c8bfc076fc166a5801bef697dfd5026a755b92cd
J-Friedemann <friedemann.jonatha@gmail.com>
2022-07-12 20:29:49 -0300

    Versão inicial

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Layout from '../components/Layout'
+import '../css/styles.css'
+
+
+
+const MyApp = ({ Component, pageProps}) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
+
+export default MyApp
